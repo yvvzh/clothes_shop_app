472cb39654a340fa6b77bc25d7ed690d9247cbec
yvvzh <135761301+yvvzh@users.noreply.github.com>
2023-10-02 16:01:55 +0200

    login UI done

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,154 @@
+import 'package:clothes_shop/components/my_button.dart';
+import 'package:clothes_shop/components/my_textfield.dart';
+import 'package:clothes_shop/components/square_tile.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  LoginPage({super.key});
+
+  // textfield controllers
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  // sign user in method
+  void signUserIn() {}
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            const SizedBox(height: 50),
+
+            // logo
+            Image.asset(
+              'lib/images/shop_logo.png',
+              height: 80,
+            ),
+
+            const SizedBox(height: 50),
+
+            // welcome back
+            Text(
+              'Welcome back USERNAME!',
+              style: TextStyle(
+                color: Colors.grey[700],
+                fontSize: 16,
+              ),
+            ),
+
+            const SizedBox(height: 25),
+
+            // username textfield
+            MyTextField(
+              controller: usernameController,
+              hintText: 'Username',
+              obscureText: false,
+            ),
+
+            const SizedBox(height: 10),
+
+            // password textfield
+            MyTextField(
+              controller: passwordController,
+              hintText: 'Password',
+              obscureText: true,
+            ),
+
+            const SizedBox(height: 10),
+
+            // mdp oublié
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'Forgot your password ?',
+                    style: TextStyle(color: Colors.grey[600]),
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 25),
+
+            // boutton de connexion
+            MyButton(
+              onTap: signUserIn,
+            ),
+
+            const SizedBox(height: 50),
+
+            // or continue with
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                      thickness: 0.5,
+                      color: Colors.grey[400],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Text(
+                      'Or continue with ...',
+                      style: TextStyle(color: Colors.grey[700]),
+                    ),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      thickness: 0.5,
+                      color: Colors.grey[400],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 50),
+
+            // google/apple logos de connexion
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // google button
+                SquareTile(imagePath: 'lib/images/google_logo.png'),
+
+                SizedBox(width: 25),
+
+                // apple button
+                SquareTile(imagePath: 'lib/images/apple_logo.png'),
+              ],
+            ),
+
+            const SizedBox(height: 40),
+
+            // not a member yet ? register now
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Not registered yet?',
+                  style: TextStyle(color: Colors.grey[700]),
+                ),
+                const SizedBox(width: 4),
+                const Text(
+                  'Register now!',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ]),
+        ),
+      ),
+    );
   }
 }
